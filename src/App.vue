eec9ed9ba318df70d1a60d1a8b3746f374b30d19
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-07-11 21:55:29 -0400

    ForecastDay component created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
 	<img alt="Vue logo" src="./assets/logo.png" />
 	<HelloWorld msg="Vue Weather App" />
+  <ForecastDay day="Monday" />
+  <ForecastDay day="Tuesday" />
+  <ForecastDay day="Wednesday" />
+  <ForecastDay day="Thursday" />
+  <ForecastDay day="Friday" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import ForecastDay from './components/ForecastDay.vue';
 
 export default {
 	name: 'App',
 	components: {
-		HelloWorld
+		HelloWorld,
+    ForecastDay
 	}
 };
 </script>
---
+++ src/components/ForecastDay.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="hello">
+		<h2>{{ day }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ForecastDay',
+	props: {
+		day: String
+	}
+};
+</script>
+<style></style>
